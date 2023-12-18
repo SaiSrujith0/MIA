300f5349e2783ba1d8cf4398c06b41445dbdad2c
SaiSrujith0 <118669934+SaiSrujith0@users.noreply.github.com>
2023-12-18 20:12:16 +0530

    Add files via upload

---
+++ F&F-M.py
@@ -0,0 +1,80 @@
+from shutil import move
+import os
+import tkinter as t
+from tkinter import *
+from tkinter import filedialog , messagebox
+
+
+w=t.Tk()
+w.resizable(False,False)
+w.geometry("380x280")
+w.title("F&F Manipulator")
+w.config(background="black")
+
+d=""
+
+def m():
+    l=Button(w,text="Select Directory",
+            command=f,
+            padx=4,
+            pady=6,
+            bg="red",
+
+            fg="black",
+            font="Ray-Ban",
+            relief="groove"
+            )
+    l.grid(row=1,column=1,
+        padx=70,
+        pady=30)
+    b=Button(w,text="Sort Files",
+             command=s,
+             padx=3,
+             pady=2,
+             fg="black",
+            bg="white",
+            font="Ray-Ban",
+            relief="raised"
+             )
+    b.grid(row=2,column=1,
+           padx=70,
+           pady=10)
+    b0=Button(w,text="Get Rid of Empty Folders",
+             command=r,
+             padx=3,
+             pady=2,
+             fg="black",
+            bg="white",
+            font="Ray-Ban",
+            relief="raised"
+             )
+    b0.grid(row=3,column=1,
+           padx=70,
+           pady=20)
+    
+
+
+def f():
+    global d
+    d=filedialog.askdirectory()
+def s():
+    for fn in os.listdir(d):
+        if os.path.isfile(os.path.join(d, fn)):
+            fe = fn.split('.')[-1]
+            des = os.path.join(d, fe)
+        if not os.path.exists(des):
+            os.makedirs(des)
+            move(os.path.join(d, fn), os.path.join(des, fn))
+    messagebox.showinfo("TRIUMPH AMIGO","Sucessfully Sorted Files Under\n"+d+" \nPath")
+
+def r():
+    for root, dirs, files in os.walk(d, topdown=False):
+        for folder in dirs:
+            folder_path = os.path.join(root, folder)
+            if not os.listdir(folder_path):
+                os.rmdir(folder_path)
+    messagebox.showinfo("TRIUMPH AMIGO",d+"\nPath Is Free of Void Folders")
+
+m()
+
+w.mainloop()
